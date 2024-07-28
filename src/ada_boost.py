fix gini impurity formula in decision tree

src/ada_boost.py
@@ -16,7 +16,7 @@ class AdaBoost:
         w = np.full(n_samples, (1 / n_samples))
 
         for _ in range(self.n_clf):
-            clf = self.base_estimator(max_depth=1)
+            clf = self.base_estimator(max_depth=3)
             clf.fit(X, y, w, pa)
 
             # make predictions and compute error
@@ -27,14 +27,18 @@ class AdaBoost:
             alpha = 0.5 * np.log((1.0 - error) / (error + 1e-10))
             clf.alpha = alpha
 
-            # update weights - e^(+/-amount of say) > 1, increases/decreases the sample weight
-            w *= np.exp(-clf.alpha * y * predictions)
+            # update weights - e^(+/-amount of say) increases/decreases the sample weight
+            # (2*y-1) * (2*predictions-1) == 1 iff predicition matches true value
+            w *= np.exp(-clf.alpha * (2*y-1) * (2*predictions-1))
             w /= np.sum(w)
 
             # save the classifier
             self.clfs.append(clf)
 
     def predict(self, X: pd.DataFrame) -> np.ndarray:
-        clf_preds = [clf.alpha * clf.predict(X) for clf in self.clfs]
-        y_pred = np.sign(np.sum(clf_preds, axis=0))
+        clf_preds = np.zeros(X.shape[0])
+        for clf in self.clfs:
+            predictions = clf.predict(X)
+            clf_preds += clf.alpha * (2*predictions-1)
+        y_pred = (clf_preds >= 0).astype(int)
         return y_pred

src/decision_tree.py
@@ -27,7 +27,7 @@ class DecisionTree:
 
     # compute gini index for left and right group
     def _compute_gini_impurity(self, val: np.ndarray, w: np.ndarray, mask):
-        total_w = np.size(w)
+        total_w = np.sum(w)
         gini = 0.0
         for indices in mask, ~mask:
             sub_tree_w = np.sum(w[indices])
@@ -35,7 +35,7 @@ class DecisionTree:
                 continue
             score = 0.0
             # Count the total weight of each class label
-            class_counts = np.bincount(val[indices], w[indices])
+            class_counts = np.bincount(val[indices], weights=w[indices])
             for count in class_counts:
                 proportion = count / sub_tree_w
                 score += proportion ** 2
@@ -53,7 +53,7 @@ class DecisionTree:
                 mask = self._split(X, feature_index, threshold)
                 gini = self._compute_gini_impurity(y, w, mask)
                 for pa in self.pa:
-                    gini -= self._compute_gini_impurity(
+                    gini += 1 - self._compute_gini_impurity(
                         X.loc[:, pa].values.flatten().astype(np.int64), w, mask)
                 if gini < best_gini:
                     best_gini, best_split = gini, {
@@ -106,7 +106,6 @@ head_X, head_y = X[:10], y[:10]
 w = np.full(10, 1/10)
 tree.fit(head_X, head_y, w, pa)
 print('train 10 records for dt: success')
-print(tree.predict(X.iloc[0:10, :]))
 
 # train on entire dataset
 w = np.full(X.shape[0], (1 / X.shape[0]))

src/experiment.py
@@ -6,6 +6,10 @@ from aif360.datasets import BinaryLabelDataset
 from aif360.metrics import BinaryLabelDatasetMetric, ClassificationMetric
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
+# just for control group
+from decision_tree import DecisionTree
+import numpy as np
+
 
 def run_exp(X: pd.DataFrame, y: pd.DataFrame, pa: list, seed=42):
     # split data
@@ -22,7 +26,7 @@ def run_exp(X: pd.DataFrame, y: pd.DataFrame, pa: list, seed=42):
     predictions = clf.predict(X_test)
     # be careful: X_test preserved original indices from X
     pred_df = pd.concat([X_test.reset_index(drop=True),
-                        pd.Series(predictions, name='labels')], axis=1)
+                        pd.Series(predictions, name='labels').reset_index(drop=True)], axis=1)
     pred_data = BinaryLabelDataset(
         df=pred_df,
         label_names=['labels'],
@@ -40,11 +44,13 @@ def run_exp(X: pd.DataFrame, y: pd.DataFrame, pa: list, seed=42):
 
     # create BinaryLabelDatasetMetric for evaluation
     binary_label_metric = BinaryLabelDatasetMetric(pred_data, privileged_groups=[
-        {pa[0]: 1}], unprivileged_groups=[{pa[0]: 0}])
+        {pa[0]: 1, pa[1]: 1}], unprivileged_groups=[{pa[0]: 0, pa[1]: 0}])
 
     # create ClassificationMetric instance
     classification_metric = ClassificationMetric(true_data, pred_data, privileged_groups=[
-        {pa[0]: 1}], unprivileged_groups=[{pa[0]: 0}])
+        {pa[0]: 1, pa[1]: 1}], unprivileged_groups=[{pa[0]: 0, pa[1]: 0}])
+
+    print('************** adaboost **************')
 
     # print fairness metrics
     print('Statistical Parity Difference:',
@@ -62,7 +68,62 @@ def run_exp(X: pd.DataFrame, y: pd.DataFrame, pa: list, seed=42):
     print('F1 Score:', f1_score(y_test, predictions))
 
 
+def run_exp_dt(X: pd.DataFrame, y: pd.DataFrame, pa: list, seed=42):
+    # split data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=seed)
+
+    # create a decision tree
+    dt = DecisionTree(max_depth=1)
+    dt.fit(X_train, y_train, np.full(
+        X_train.shape[0], 1.0 / X_train.shape[0]), pa)
+    tree_pred = dt.predict(X_test)
+
+    # be careful: X_test preserved original indices from X
+    pred_df = pd.concat([X_test.reset_index(drop=True),
+                        pd.Series(tree_pred.reset_index(drop=True), name='labels')], axis=1)
+    pred_data = BinaryLabelDataset(
+        df=pred_df,
+        label_names=['labels'],
+        protected_attribute_names=pa
+    )
+
+    # create BinaryLabelDataset for true data
+    true_df = pd.concat([X_test.reset_index(drop=True),
+                        y_test.reset_index(drop=True)], axis=1)
+    true_data = BinaryLabelDataset(
+        df=true_df,
+        label_names=['labels'],
+        protected_attribute_names=pa
+    )
+
+    # create BinaryLabelDatasetMetric for evaluation
+    binary_label_metric = BinaryLabelDatasetMetric(pred_data, privileged_groups=[
+        {pa[0]: 1, pa[1]: 1}], unprivileged_groups=[{pa[0]: 0, pa[1]: 0}])
+
+    # create ClassificationMetric instance
+    classification_metric = ClassificationMetric(true_data, pred_data, privileged_groups=[
+        {pa[0]: 1, pa[1]: 1}], unprivileged_groups=[{pa[0]: 0, pa[1]: 0}])
+
+    print('************** decision tree **************')
+
+    # print fairness metrics
+    print('Statistical Parity Difference:',
+          binary_label_metric.statistical_parity_difference())
+    print('Disparate Impact:', binary_label_metric.disparate_impact())
+    print('Average Odds Difference:',
+          classification_metric.average_odds_difference())
+    print('Equal Opportunity Difference:',
+          classification_metric.equal_opportunity_difference())
+
+    print('Accuracy Score:', accuracy_score(y_test, tree_pred))
+    print('Precision Score:', precision_score(y_test, tree_pred))
+    print('Recall Score:', recall_score(y_test, tree_pred))
+    print('F1 Score:', f1_score(y_test, tree_pred))
+
+
 data = load_adult_data()
 X, y = data['X'], data['y']
 pa = data['pa']
 run_exp(X, y, pa)
+run_exp_dt(X, y, pa)